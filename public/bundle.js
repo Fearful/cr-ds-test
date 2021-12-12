e984f4483e645e9265762c73c3c06dc4b96d40a6
unknown <joan.lascano@gmail.com>
2021-12-11 23:24:39 -0300

    hardcode bundle for now

---
+++ public/bundle.js
@@ -0,0 +1,2 @@
+var app=function(e,t){"use strict";function u(e){return e&&"object"==typeof e&&"default"in e?e:{default:e}}var r=u(t);return Object.defineProperty(e,"ImageFullWidth",{enumerable:!0,get:function(){return r.default}}),Object.defineProperty(e,"__esModule",{value:!0}),e}({},ImageFullWidth);
+//# sourceMappingURL=bundle.js.map
